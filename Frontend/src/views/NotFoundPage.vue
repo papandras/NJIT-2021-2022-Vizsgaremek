276e4259d871706801e723d30d2b5e9258c73dea
farkaspeter14b <90248561+farkaspeter14b@users.noreply.github.com>
2022-03-31 11:54:41 +0200

    notfoundpage
    error

--- Frontend/src/views/NotFoundPage.vue
+++ Frontend/src/views/NotFoundPage.vue
@@ -1,3 +1,52 @@
 <template>
     <h1>Az oldal nem található</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col bgc">
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="col LOGO">
+                <img src="public/logo.svg" alt="logo" class="roundlogo">
+            </div>
+        </div>
+        <div class="row">
+            <div class="bgc">
+                <h3 class="tittle">
+                <i>444</i>
+                </h3>
+            </div>
+        </div>
+    </div>
+        
 </template>
+
+<style>
+h1{
+    text-align: center;
+}
+.tittle{
+    text-align: center;
+    font-size: 40px;
+}
+.bgc{
+    height: 50px;
+    background-color: #C4C4C4;
+}
+.LOGO{
+    display: block;
+    margin-right: auto;
+    margin-left: auto;
+}
+.roundlogo{
+    height: 300px;
+    width: 400px;
+    display: block;
+    margin-right: auto;
+    margin-left: auto;
+    border-radius: 500px;
+    border: solid 5px black;
+
+}
+</style>
