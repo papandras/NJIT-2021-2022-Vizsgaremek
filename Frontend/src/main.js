538cd67876b2ad0ae2801616ee61a7ff09b31124
papandras <papandras01@gmail.com>
2022-04-07 21:28:11 +0200

    axios installed

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -2,10 +2,13 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { createPinia } from "pinia";
 import router from "./router";
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(VueAxios, axios);
 
 app.mount("#app");
