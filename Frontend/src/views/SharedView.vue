bb54398cdbe296904a5df8a349e94866736aa54e
papandras <papandras01@gmail.com>
2022-04-21 21:57:57 +0200

    shared files page - started

--- Frontend/src/views/SharedView.vue
+++ Frontend/src/views/SharedView.vue
@@ -1,67 +1,64 @@
 <template>
   <div class="shared">
     <Menu id="menu" activepage="shared"></Menu>
-    <div id="content">   
+    <div id="content">
+      <LastFilesTable v-for="group in groups" :key="group.id" :lastfilesobject="ngdfgsdull" :refresh="nusgddll" :title="group.name" id="groups" name="groups" />
     </div>
   </div>
 </template>
 
 <script>
 import Menu from "../components/LeftSideMenu.vue";
-import Datas from "../components/settings/setDatas.vue";
-import Security from "../components/settings/setSecurity.vue";
+import axios from "axios";
+import { useAuth } from "../store/auth.js";
+import LastFilesTable from "../components/LastFilesTable.vue";
 
 export default {
   components: {
     Menu,
-    Datas,
-    Security
+    LastFilesTable
   },
   data() {
     return {
-      showdata: true,
-      showsecurity: false
+      store: useAuth(),
+      groups: null,
     }
   },
   methods: {
-    displayDatas() {
-      this.showdata = true;
-      this.showsecurity = false;
-    },
-    displaySecurity() {
-      this.showdata = false;
-      this.showsecurity = true;
+    async loadgroups() {
+      await axios
+        .get(`http://localhost:8881/api/user/${this.store.user.id}/groups`, { withCredentials: true })
+        .then((response) => {
+          this.groups = response.data.data;
+          console.log(response.data)
+        })
     }
+  },
+  async mounted() {
+    await axios
+      .get("http://localhost:8881/api/user", { withCredentials: true })
+      .then((response) => {
+        this.store.user = response.data;
+      });
+    await this.loadgroups()
   }
 };
 </script>
 
 <style scoped>
-.shared {
+.settings {
   display: grid;
   grid-template-columns: 10% 90%;
   grid-template-rows: 100%;
   grid-template-areas: "menu content";
 }
+
+#menu {
+  grid-area: menu;
+}
+
 #content {
   padding: 20px;
+  grid-area: content;
 }
-
-#whiteBackground {
-  position: relative;
-}
-
-#save {
-  position: absolute;
-  right: 20px;
-  bottom:  20px;
-}
-
-#right, #left {
-  padding: 10px;
-}
-</style>
-
-<style scoped>
-@import url("../assets/css/SettingsView.css");
 </style>
