26cb8066fa0454ac087e2f9926cd1da42a149cfb
farkaspeter14b <farkas.peter.55@gmail.com>
2022-04-21 05:11:04 +0200

    Create SharedFiles.vue

---
+++ Frontend/src/views/SharedFiles.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="settings">
+    <Menu id="menu" activepage="index"></Menu>
+    <div id="content">
+
+    </div>
+  </div>
+</template>
+
+<script>
+import Menu from "../components/LeftSideMenu.vue";
+
+export default {
+  components: {
+    Menu,
+  },
+};
+</script>
+
+<style scoped>
+.settings {
+  display: grid;
+  grid-template-columns: 10% 90%;
+  grid-template-rows: 100%;
+  grid-template-areas: "menu content";
+}
+#content {
+  padding: 20px;
+}
+
+</style>
