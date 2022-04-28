5bdb682f1314aaeec9f578ea84450c20b24b1ba0
papandras <papandras01@gmail.com>
2022-04-28 08:02:46 +0200

    update installs

--- Backend/install.bat
+++ Backend/install.bat
@@ -6,4 +6,5 @@ docker-compose exec php php artisan migrate
 docker-compose exec php php artisan storage:link
 docker-compose exec php php artisan make:mail OrderShipped
 docker-compose exec php php artisan db:seed --class=AdminUserSeeder
+md storage\app\profilpic
 copy public\base-notfound.svg storage\app\profilpic
