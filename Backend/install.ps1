8d79c5cda0eb7061a275fcb2f1e0e6276a2b4fb6
papandras <papandras01@gmail.com>
2022-04-29 22:33:54 +0200

    installs final

--- Backend/install.ps1
+++ Backend/install.ps1
@@ -2,10 +2,8 @@ Copy-Item .env.example .env
 docker-compose up -d
 docker-compose exec php composer install
 docker-compose exec php php artisan key:generate
-docker-compose exec php php artisan migrate
 docker-compose exec php php artisan storage:link
 docker-compose exec php php artisan make:mail OrderShipped
-docker-compose exec php php artisan db:seed --class=AdminUserSeeder
 md storage\app\profilpic
 Copy-Item public\base-notfound.svg storage\app\profilpic
 md storage\app\storage
