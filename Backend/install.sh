4da87b8110711df64a80187fd893e7d711a77d21
papandras <papandras01@gmail.com>
2022-04-30 20:47:18 +0200

    Update install.sh

--- Backend/install.sh
+++ Backend/install.sh
@@ -4,8 +4,8 @@ docker-compose exec php composer install
 docker-compose exec php php artisan key:generate
 docker-compose exec php php artisan storage:link
 docker-compose exec php php artisan make:mail OrderShipped
-md storage\app\profilpic
-cp public\base-notfound.svg storage\app\profilpic
-md storage\app\storage
-cp public\files.zip storage\app\storage
-unzip -q storage\app\storage\files.zip
+mkdir -p storage/app/profilpic
+cp public/base-notfound.svg storage/app/profilpic
+mkdir -p storage/app/storage
+cp public/files.zip storage/app/storage
+unzip -q storage/app/storage/files.zip
